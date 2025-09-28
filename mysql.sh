b0f792566c5d652f5c09553764ca87d70a2ac8f2
nageswar8 <nageswarreddy447@gmail.com>
2025-09-28 10:44:00 +0530

    shell script

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 app_name="mysql"
-install_software="mysql-server"
+# install_software="mysql-server"
 software_name="mysqld"
 
 source ./common.sh
 
 CHECK_ROOT
 
-install
+install "mysql-server"
 software_start
 
 mysql_secure_installation --set-root-pass RoboShop@1
